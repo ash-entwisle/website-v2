0d4c953253c8a9548839e05f2d40aa382a74afcc
Ash Entwisle <ash.entwisle@mailfence.com>
2023-03-17 09:46:54 +0000

    forgot to remove main tag
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,14 +1,12 @@
 export default function Index() {
     return (
-        <main>
-            <div class="index">
-                <h1>Hello, World!</h1>
-                <h2>I'm Ash and I sometimes make software.</h2>
-                <p>
-                    Enjoyer of Arch, Lover of Rust, hater of JavaScript. 
-                    Feel free to check out my <a href="/projects">projects</a> or my <a href="/blog">blog</a>. 
-                </p>
-            </div>
-        </main>
+        <div class="index">
+            <h1>Hello, World!</h1>
+            <h2>I'm Ash and I sometimes make software.</h2>
+            <p>
+                Enjoyer of Arch, Lover of Rust, hater of JavaScript. 
+                Feel free to check out my <a href="/projects">projects</a> or my <a href="/blog">blog</a>. 
+            </p>
+        </div>
     );
 }
