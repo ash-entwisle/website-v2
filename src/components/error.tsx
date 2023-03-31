df68c7d03f4ca57d52888787e44533594ae5ced7
Ash Entwisle <ash.entwisle@mailfence.com>
2023-03-31 14:05:54 +0100

    fixed blog post issues
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,11 +1,9 @@
 
 export default function Error() {
     return (
-        <main>
-            <div class="error">
-                <h1>ERR: 404</h1> 
-                <p>Sorry, the page `<code>https://ashentwisle.com{window.location.pathname}</code>` does not exist. <i>Click <a href="/">here</a> to go home</i></p>
-            </div>
-        </main>
+        <div class="error">
+            <h1>ERR: 404</h1> 
+            <p>Sorry, the page `<code>https://ashentwisle.com{window.location.pathname}</code>` does not exist. <i>Click <a href="/">here</a> to go home</i></p>
+        </div>
     );
 }
